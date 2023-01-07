cb9ebe4787a5f0de2c561dd6864b443dccfab68d
febe89 <fullstack9020@gmail.com>
2023-01-07 11:58:58 +0530

    Deployed website

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-
 const AddUser = () => {
   let navigate = useNavigate()
   const [users, setUsers] = useState({
